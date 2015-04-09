a087307a1fcc5faa92d7215a2c72d1f14ab2a9a6
fred <fraiid.b@gmail.com>
2015-04-09 17:44:47 +0200

    modif: demandeVisiteSpec

--- grails-app/domain/projetjavaee/DemandeVisite.groovy
+++ grails-app/domain/projetjavaee/DemandeVisite.groovy
@@ -8,5 +8,10 @@ class DemandeVisite {
     String statut
 
     static constraints = {
+        code blank: false, nullable: true
+        dateDebutPeriode blank: false, nullable: false
+        dateFinPeriode blank: false, nullable: false
+        nbPersonnes range: 1..6, nullable: false
+        statut blank: false, inList: ["En cours de traitement", "Confirmee", "Refusee"], nullable: true
     }
 }
--- test/unit/projetjavaee/DemandeVisiteSpec.groovy
+++ test/unit/projetjavaee/DemandeVisiteSpec.groovy
@@ -2,6 +2,7 @@ package projetjavaee
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,12 +10,36 @@ import spock.lang.Specification
 @TestFor(DemandeVisite)
 class DemandeVisiteSpec extends Specification {
 
-    def setup() {
+    @Unroll
+    void "test la validite d'une demande de visite valide"(String unCode,Date uneDateDebut, Date uneDateFin, int unNbPersonnes, String unStatut) {
+
+        given: "une demande de visite initialise avec une date de debut non vide , une date de fin de période non vide, un nombre de personnes présente inférieur à 6"
+        DemandeVisite demandeVisite = new DemandeVisite(code: unCode, dateDebutPeriode: uneDateDebut, dateFinPeriode: uneDateFin, nbPersonnes: unNbPersonnes, statut: unStatut)
+
+
+        expect: "la demande de visite est valide"
+        demandeVisite.validate() == true
+
+        where:
+        unCode | uneDateDebut          | uneDateFin            | unNbPersonnes | unStatut
+        _ | new Date(2015, 4, 15) | new Date(2015, 4, 20) | 5 | _
     }
 
-    def cleanup() {
-    }
+    @Unroll
+    void "test l'invalidite d'une demande de visite non valide"(String unCode, Date uneDateDebut, Date uneDateFin, int unNbPersonnes, String unStatut) {
 
-    void "test something"() {
+        given: "une demande de visite initialise de maniere non valide"
+        DemandeVisite demandeVisite = new DemandeVisite(code: unCode, dateDebutPeriode: uneDateDebut, dateFinPeriode: uneDateFin, nbPersonnes: unNbPersonnes, statut: unStatut)
+
+        expect: "la demande de visite est invalide"
+        demandeVisite.validate() == false
+
+        where:
+        unCode | uneDateDebut          | uneDateFin            | unNbPersonnes | unStatut
+        _      | new Date(2015, 4, 15) | new Date(2015, 4, 20) | 0             | _
+        _      | new Date(2015, 4, 15) | new Date(2015, 4, 20) | 7             | _
+        _      | null                  | new Date(2015, 4, 20) | 5             | _
+        _      | new Date(2015, 4, 15) | null                  | 5             | _
+        _      | new Date(2015, 4, 20) | new Date(2015, 4, 15) | 5             | _
     }
 }
