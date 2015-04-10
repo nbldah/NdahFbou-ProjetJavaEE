4a83bb07e98cbda02e66318d531b5fcff49635ae
nbldah <nabildahalani@hotmail.fr>
2015-04-10 08:51:00 +0200

    Test Musee

--- test/unit/projetjavaee/DemandeVisiteMuseeSpec.groovy
+++ test/unit/projetjavaee/DemandeVisiteMuseeSpec.groovy
@@ -15,13 +15,9 @@ class DemandeVisiteMuseeSpec extends Specification {
     void "test la validite d'une demande de visite d'un musée valide"() {
 
         given:"un musee et une activité"
-        Adresse uneAdresse = new Adresse(numero: "40", rue: "jean jaures", codePostal: 31400, ville: "Toulouse")
-        Musee unMusee = new Musee(nom: "unNom", horaireOuverture: "12h à 14h",telephone: 0534345434,
-        accesBus: "bus 2",accesMetro: "metro 2",adresse:uneAdresse)
 
-        DemandeVisite uneDemandeVisite = new DemandeVisite(code: "86",dateDebutPeriode: new Date(),
-                dateFinPeriode: new Date(),nbPersonnes: 4 ,statut: "valide")
-
+        Musee unMusee = Mock(Musee)
+        DemandeVisite uneDemandeVisite = Mock(DemandeVisite)
 
         and:"une date"
         Date now = new Date()
@@ -44,12 +40,8 @@ class DemandeVisiteMuseeSpec extends Specification {
     void "test l'invalidite d'une demande de visite de musee non valide"() {
 
         given:"un musee et une activité"
-        Adresse uneAdresse = new Adresse(numero: "40", rue: "jean jaures", codePostal: 31400, ville: "Toulouse")
-        Musee unMusee = new Musee(nom: "unNom", horaireOuverture: "12h à 14h",telephone: 0534345434,
-                accesBus: "bus 2",accesMetro: "metro 2",adresse:uneAdresse)
-
-        DemandeVisite uneDemandeVisite = new DemandeVisite(code: "86",dateDebutPeriode: new Date(),
-                dateFinPeriode: new Date(),nbPersonnes: 4 ,statut: "valide")
+        Musee unMusee = Mock(Musee)
+        DemandeVisite uneDemandeVisite = Mock(DemandeVisite)
 
         and:"une date"
         Date now = new Date()
--- test/unit/projetjavaee/MuseeSpec.groovy
+++ test/unit/projetjavaee/MuseeSpec.groovy
@@ -2,6 +2,7 @@ package projetjavaee
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,6 +10,26 @@ import spock.lang.Specification
 @TestFor(Musee)
 class MuseeSpec extends Specification {
 
+    @Unroll
+    void "test la validite d'un Musee valide"(String unNom, String uneHoraireOuverture,
+                                              String unTelephone , String unAccesMetro,
+                                              String unAccesBus , Adresse uneAdresse) {
+
+        given: "un Musee initialise correctement"
+        Musee unMusee = new Musee(nom: unNom, horaireOuverture: uneHoraireOuverture
+                , telephone: unTelephone, accesMetro: unAccesMetro, accesBus: unAccesBus , adresse: uneAdresse )
+
+        expect: "le mussee est valide"
+        unMusee.validate() == true
+
+        where:
+
+        unNom                   | uneHoraireOuverture  | unTelephone  | unAccesMetro | unAccesBus | uneAdresse
+        "Musee des compagnons"  | "de 8h à 17h"        | "0568998767" | "metro B"    | "bus 178"  | Mock(Adresse)
+        "Archive municipale"    | "de 8h à 18h"        | "0534768965" | "metro A"    | "bus 178"  | Mock(Adresse)
+
+
+    }
 
 
 
