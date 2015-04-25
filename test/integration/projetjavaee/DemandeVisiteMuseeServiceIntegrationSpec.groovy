568a33343e8471e01c7c207ddac2dcc993b76fb6
fred b <fraiid.b@gmail.com>
2015-04-26 00:57:04 +0200

    modif DemandeVisiteMuseeServiceIntegration - code postal d'adresse en string

--- test/integration/projetjavaee/DemandeVisiteMuseeServiceIntegrationSpec.groovy
+++ test/integration/projetjavaee/DemandeVisiteMuseeServiceIntegrationSpec.groovy
@@ -16,7 +16,7 @@ class DemandeVisiteMuseeServiceIntegrationSpec extends Specification {
     MuseeService museeService
 
     def setup() {
-        Adresse uneAdresse = new Adresse(numero: "2", rue: "RUE DES ARCHIVES", codePostal: 31500, ville: "Toulouse")
+        Adresse uneAdresse = new Adresse(numero: "2", rue: "RUE DES ARCHIVES", codePostal: "31500", ville: "Toulouse")
         Gestionnaire unGestionnaire = new Gestionnaire(nom: "Mairie de Toulouse - DGA Culture")
         unMusee = new Musee(nom: "ARCHIVES MUNICIPALES TOULOUSE", horaireOuverture: "Ouvert du lundi au vendredi de 9h à 17h. Fermeture de 12h à 13h30 pendant toutes les vacances scolaires. Fermeture annuelle la dernière quinzaine de juillet.", telephone: "05 61 61 63 33", accesMetro: "Roseraie (A)", accesBus: "36, 38")
 
