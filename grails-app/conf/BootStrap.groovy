7dc1fe0d2e49cab9c84fcbb6f843e55bc44296b0
fred b <fraiid.b@gmail.com>
2015-04-26 00:26:57 +0200

    modif bootstrap init jeuTest

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
+import projetjavaee.JeuTestService
+
 class BootStrap {
 
+    JeuTestService jeuTestService
+
     def init = { servletContext ->
+        jeuTestService.createJeuTestAdresse()
+        jeuTestService.createJeuTestGestionnaire()
+        jeuTestService.createJeuTestMusee()
     }
     def destroy = {
     }
