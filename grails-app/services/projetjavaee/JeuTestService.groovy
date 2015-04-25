23c19a299c00dfc6e87d9453b64102e05cbf1911
nbldah <nabildahalani@hotmail.fr>
2015-04-25 21:43:06 +0200

    JeuTestService ajout transaction

--- grails-app/services/projetjavaee/JeuTestService.groovy
+++ grails-app/services/projetjavaee/JeuTestService.groovy
@@ -10,6 +10,8 @@ class JeuTestService {
     Gestionnaire gestionnaire3
     Gestionnaire gestionnaire4
 
+    MuseeService museeService
+
     Adresse adresse1
     Adresse adresse2
     Adresse adresse3
@@ -36,42 +38,67 @@ class JeuTestService {
     Musee musee11
     Musee musee12
 
-    def createJeuTest() {
+    def createJeuTestGestionnaire() {
+
         if (Gestionnaire.count() == 0) {
-            gestionnaire1 = new Gestionnaire(nom: "Mairie de Toulouse - DGA Culture")
-            gestionnaire2 = new Gestionnaire(nom: "Association")
-            gestionnaire3 = new Gestionnaire(nom: "Autre institution publique")
-            gestionnaire4 = new Gestionnaire(nom: "Structure commercialee")
+            gestionnaire1 = new Gestionnaire(nom: "Mairie de Toulouse - DGA Culture").save()
+            gestionnaire2 = new Gestionnaire(nom: "Association").save()
+            gestionnaire3 = new Gestionnaire(nom: "Autre institution publique").save()
+            gestionnaire4 = new Gestionnaire(nom: "Structure commercialee").save()
         }
+
+    }
+
+    def createJeuTestAdresse() {
+
 
         if (Adresse.count() == 0) {
-            adresse1 = new Adresse(numero: "2", rue: "RUE DES ARCHIVES", codePostal: 31500, ville: "Toulouse")
-            adresse2 = new Adresse(numero: "5", rue: "RUE SAINT PANTALEON", codePostal: 31000, ville: "Toulouse")
-            adresse3 = new Adresse(numero: "69", rue: "RUE PARGAMINIERES", codePostal: 31000, ville: "Toulouse")
-            adresse4 = new Adresse(numero: "31", rue: "RUE DE LA FONDERIE", codePostal: 31000, ville: "Toulouse")
-            adresse5 = new Adresse(numero: "TOUT", rue: "RUE MONTMORENCY", codePostal: 31200, ville: "Toulouse")
-            adresse6 = new Adresse(numero: "2", rue: "RUE VIGUERIE", codePostal: 31300, ville: "Toulouse")
-            adresse7 = new Adresse(numero: "21", rue: "RUE DE METZ", codePostal: 3100, ville: "Toulouse")
-            adresse8 = new Adresse(numero: "2", rue: "RUE TRIPIERE", codePostal: 31000, ville: "Toulouse")
-            adresse9 = new Adresse(numero: "2", rue: "RUE VIGUERIE", codePostal: 31300, ville: "Toulouse")
-            adresse10 = new Adresse(numero: "7", rue: "RUE DU MAY", codePostal: 31000, ville: "Toulouse")
-            adresse11 = new Adresse(numero: "TOUT", rue: "RUE DU JAPON", codePostal: 31400, ville: "Toulouse")
-            adresse12 = new Adresse(numero: "13", rue: "RUE DE LA PLEAU", codePostal: 31000, ville: "Toulouse")
+            adresse1 = new Adresse(numero: "2", rue: "RUE DES ARCHIVES", codePostal: 31500, ville: "Toulouse").save()
+            adresse2 = new Adresse(numero: "5", rue: "RUE SAINT PANTALEON", codePostal: 31000, ville: "Toulouse").save()
+            adresse3 = new Adresse(numero: "69", rue: "RUE PARGAMINIERES", codePostal: 31000, ville: "Toulouse").save()
+            adresse4 = new Adresse(numero: "31", rue: "RUE DE LA FONDERIE", codePostal: 31000, ville: "Toulouse").save()
+            adresse5 = new Adresse(numero: "TOUT", rue: "RUE MONTMORENCY", codePostal: 31200, ville: "Toulouse").save()
+            adresse6 = new Adresse(numero: "2", rue: "RUE VIGUERIE", codePostal: 31300, ville: "Toulouse").save()
+            adresse7 = new Adresse(numero: "21", rue: "RUE DE METZ", codePostal: 3100, ville: "Toulouse").save()
+            adresse8 = new Adresse(numero: "2", rue: "RUE TRIPIERE", codePostal: 31000, ville: "Toulouse").save()
+            adresse9 = new Adresse(numero: "2", rue: "RUE VIGUERIE", codePostal: 31300, ville: "Toulouse").save()
+            adresse10 = new Adresse(numero: "7", rue: "RUE DU MAY", codePostal: 31000, ville: "Toulouse").save()
+            adresse11 = new Adresse(numero: "TOUT", rue: "RUE DU JAPON", codePostal: 31400, ville: "Toulouse").save()
+            adresse12 = new Adresse(numero: "13", rue: "RUE DE LA PLEAU", codePostal: 31000, ville: "Toulouse").save()
         }
+    }
 
-        if (Musee.count() == 0) {
-            musee1 = new Musee(nom: "ARCHIVES MUNICIPALES TOULOUSE", horaireOuverture: "Ouvert du lundi au vendredi de 9h à 17h. Fermeture de 12h à 13h30 pendant toutes les vacances scolaires. Fermeture annuelle la dernière quinzaine de juillet.", telephone: "05 61 61 63 33", accesMetro: "Roseraie (A)", accesBus: "36, 38", adresse: adresse1)
-            musee2 = new Musee(nom: "CMAV - CENTRE MERIDIONAL DE L'ARCHITECTURE DE LA VILLE", horaireOuverture: "Ouvert du mardi au samedi de 13h à 19hfermé les dimanches, jours fériés et du 1er au 15 août", telephone: "05 61 23 30 49", accesMetro: "Capitole (A), Jean Jaurès (B)", accesBus: "ncv, 2, 10, 12, 14, 38, 78 et 80", adresse: adresse2)
-            musee3 = new Musee(nom: "ENSEMBLE CONVENTUEL DES JACOBINS", horaireOuverture: "Ouvert tous les jours de 9h à 19h.", telephone: "05 61 22 21 92", accesMetro: "Esquirol, Capitole (A)", accesBus: "ncv, 2, 10, 12, 14, 38, 78, 80", adresse: adresse3)
-            musee4 = new Musee(nom: "INSTITUT CATHOLIQUE DE TOULOUSE - ESPACE MUSEOGRAPHIQUE BACCRABERE - SALLE TOLOSA", horaireOuverture: "Ouvert le premier vendredi de chaque mois de 18h à 20h.", telephone: "05 61 36 81 12", accesMetro: "Carmes (B)", accesBus: "2, 38", adresse: adresse4)
-            musee5 = new Musee(nom: "L'AEROTHEQUE", horaireOuverture: "Ouvert le lundi et le mercredi de 14h à 17h et le mardi de 9h à 12h", telephone: "05 61 93 93 57", accesMetro: "Canal du Midi (B)", accesBus: "15", adresse: adresse5)
-            musee6 = new Musee(nom: "MUSEE DE L'HISTOIRE DE LA MEDECINE DE TOULOUSE", horaireOuverture: "Ouvert tous les jeudi et vendredi de 11h à 17h, et le dimande de 10h à 18h.Visites guidées sur demande.", telephone: "05 61 77 84 25", accesMetro: "Saint-Cyprien-République, Esquirol (A)", accesBus: "2, 10, 12, 14, 78, 80", adresse: adresse6)
-            musee7 = new Musee(nom: "MUSEE DES AUGUSTINS - MUSEE DES BEAUX ARTS DE TOULOUSE", horaireOuverture: "Tous les jours : 10h - 18h /  nocturne le mercredi jusqu'à 21h.", telephone: "05 61 22 21 82", accesMetro: "Esquirol (A), Carmes (B)", accesBus: "ncv, 2, 10, 14, 38, 78, 80", adresse: adresse7)
-            musee8 = new Musee(nom: "MUSEE DES COMPAGNONS", horaireOuverture: "Le Mercredi et le 1er dimanche de chaque mois de 14h à 17h", telephone: "05 62 47 41 77", accesMetro: "Esquirol, Capitole (A)", accesBus: "2, 10, 12, 14, 38, 78, 80", adresse: adresse8)
-            musee9 = new Musee(nom: "MUSEE DES INSTRUMENTS DE MEDECINE DES HOPITAUX DE TOULOUSE", horaireOuverture: "Ouvert tous les jeudi et vendredi de 13h à 17h. Ouvert le premier dimanche du mois.Visites guidées sur demande.", telephone: "05 61 77 82 72", accesMetro: "Saint-Cyprien-République, Esquirol (A)", accesBus: "2, 10, 12, 14, 78, 80", adresse: adresse9)
-            musee10 = new Musee(nom: "MUSEE DU VIEUX TOULOUSE", horaireOuverture: "Ouvert tous les jours du 2 mai au 31 octobre de 14h00 à 18h00.Fermé le dimanche et jours fériés.", telephone: "05 62 27 11 50", accesMetro: "Esquirol, Capitole (A)", accesBus: "2, 10, 12, 14, 38, 78, 80", adresse: adresse10)
-            musee11 = new Musee(nom: "MUSEE GEORGES-LABIT", horaireOuverture: "Musée ouvert de 10h à 17h en hiver et de 10h à 18h en été (du 1er juin au 30 septembre). Fermeture hebdomadaire le mardi.Bibliothèque ouverte le lundi de 14h à 17h, le mardi et le mercredi : 9h-12h et 14h-17h.", telephone: "05 61 14 65 50", accesMetro: "François Verdier (B)", accesBus: "10, 2, 27", adresse: adresse11)
-            musee12 = new Musee(nom: "MUSEE PAUL-DUPUY - ARTS GRAPHIQUES ET ARTS DECORATIFS", horaireOuverture: "Ouvert de 10h - 17h en hiver et de 10h à 18h en été (du 1er juin au 30 septembre). Fermé le mardi et jours fériés.", telephone: "05 61 14 65 50", accesMetro: "Esquirol (A), Carmes (B)", accesBus: "ncv, 2, 12, 52", adresse: adresse12)
-        }
+
+        def createJeuTestMusee() {
+
+            if (Musee.count() == 0) {
+            musee1 = new Musee(nom: "ARCHIVES MUNICIPALES TOULOUSE", horaireOuverture: "Ouvert du lundi au vendredi de 9h à 17h. Fermeture de 12h à 13h30 pendant toutes les vacances scolaires. Fermeture annuelle la dernière quinzaine de juillet.", telephone: "05 61 61 63 33", accesMetro: "Roseraie (A)", accesBus: "36, 38")
+            musee2 = new Musee(nom: "CMAV - CENTRE MERIDIONAL DE L'ARCHITECTURE DE LA VILLE", horaireOuverture: "Ouvert du mardi au samedi de 13h à 19hfermé les dimanches, jours fériés et du 1er au 15 août", telephone: "05 61 23 30 49", accesMetro: "Capitole (A), Jean Jaurès (B)", accesBus: "ncv, 2, 10, 12, 14, 38, 78 et 80")
+            musee3 = new Musee(nom: "ENSEMBLE CONVENTUEL DES JACOBINS", horaireOuverture: "Ouvert tous les jours de 9h à 19h.", telephone: "05 61 22 21 92", accesMetro: "Esquirol, Capitole (A)", accesBus: "ncv, 2, 10, 12, 14, 38, 78, 80")
+            musee4 = new Musee(nom: "INSTITUT CATHOLIQUE DE TOULOUSE - ESPACE MUSEOGRAPHIQUE BACCRABERE - SALLE TOLOSA", horaireOuverture: "Ouvert le premier vendredi de chaque mois de 18h à 20h.", telephone: "05 61 36 81 12", accesMetro: "Carmes (B)", accesBus: "2, 38")
+            musee5 = new Musee(nom: "L'AEROTHEQUE", horaireOuverture: "Ouvert le lundi et le mercredi de 14h à 17h et le mardi de 9h à 12h", telephone: "05 61 93 93 57", accesMetro: "Canal du Midi (B)", accesBus: "15")
+            musee6 = new Musee(nom: "MUSEE DE L'HISTOIRE DE LA MEDECINE DE TOULOUSE", horaireOuverture: "Ouvert tous les jeudi et vendredi de 11h à 17h, et le dimande de 10h à 18h.Visites guidées sur demande.", telephone: "05 61 77 84 25", accesMetro: "Saint-Cyprien-République, Esquirol (A)", accesBus: "2, 10, 12, 14, 78, 80")
+            musee7 = new Musee(nom: "MUSEE DES AUGUSTINS - MUSEE DES BEAUX ARTS DE TOULOUSE", horaireOuverture: "Tous les jours : 10h - 18h /  nocturne le mercredi jusqu'à 21h.", telephone: "05 61 22 21 82", accesMetro: "Esquirol (A), Carmes (B)", accesBus: "ncv, 2, 10, 14, 38, 78, 80")
+            musee8 = new Musee(nom: "MUSEE DES COMPAGNONS", horaireOuverture: "Le Mercredi et le 1er dimanche de chaque mois de 14h à 17h", telephone: "05 62 47 41 77", accesMetro: "Esquirol, Capitole (A)", accesBus: "2, 10, 12, 14, 38, 78, 80")
+            musee9 = new Musee(nom: "MUSEE DES INSTRUMENTS DE MEDECINE DES HOPITAUX DE TOULOUSE", horaireOuverture: "Ouvert tous les jeudi et vendredi de 13h à 17h. Ouvert le premier dimanche du mois.Visites guidées sur demande.", telephone: "05 61 77 82 72", accesMetro: "Saint-Cyprien-République, Esquirol (A)", accesBus: "2, 10, 12, 14, 78, 80")
+            musee10 = new Musee(nom: "MUSEE DU VIEUX TOULOUSE", horaireOuverture: "Ouvert tous les jours du 2 mai au 31 octobre de 14h00 à 18h00.Fermé le dimanche et jours fériés.", telephone: "05 62 27 11 50", accesMetro: "Esquirol, Capitole (A)", accesBus: "2, 10, 12, 14, 38, 78, 80")
+            musee11 = new Musee(nom: "MUSEE GEORGES-LABIT", horaireOuverture: "Musée ouvert de 10h à 17h en hiver et de 10h à 18h en été (du 1er juin au 30 septembre). Fermeture hebdomadaire le mardi.Bibliothèque ouverte le lundi de 14h à 17h, le mardi et le mercredi : 9h-12h et 14h-17h.", telephone: "05 61 14 65 50", accesMetro: "François Verdier (B)", accesBus: "10, 2, 27")
+            musee12 = new Musee(nom: "MUSEE PAUL-DUPUY - ARTS GRAPHIQUES ET ARTS DECORATIFS", horaireOuverture: "Ouvert de 10h - 17h en hiver et de 10h à 18h en été (du 1er juin au 30 septembre). Fermé le mardi et jours fériés.", telephone: "05 61 14 65 50", accesMetro: "Esquirol (A), Carmes (B)", accesBus: "ncv, 2, 12, 52")
+
+
+            museeService.insertOrUpdateMusee(musee1,adresse1,gestionnaire1)
+            museeService.insertOrUpdateMusee(musee2,adresse2,gestionnaire2)
+            museeService.insertOrUpdateMusee(musee3,adresse3,gestionnaire1)
+            museeService.insertOrUpdateMusee(musee4,adresse4,gestionnaire3)
+            museeService.insertOrUpdateMusee(musee5,adresse5,gestionnaire4)
+            museeService.insertOrUpdateMusee(musee6,adresse6,gestionnaire2)
+            museeService.insertOrUpdateMusee(musee7,adresse7,gestionnaire1)
+            museeService.insertOrUpdateMusee(musee8,adresse8,gestionnaire2)
+            museeService.insertOrUpdateMusee(musee9,adresse9,gestionnaire4)
+            museeService.insertOrUpdateMusee(musee10,adresse10,gestionnaire2)
+            museeService.insertOrUpdateMusee(musee11,adresse11,gestionnaire1)
+            museeService.insertOrUpdateMusee(musee12,adresse12,gestionnaire1)
+
+            }
     }
 }
--- test/integration/projetjavaee/JeuTestServiceIntegrationSpec.groovy
+++ test/integration/projetjavaee/JeuTestServiceIntegrationSpec.groovy
@@ -8,8 +8,35 @@ import spock.lang.*
  */
 class JeuTestServiceIntegrationSpec extends Specification {
 
-    def jeuTestService
 
-    void "test creation du jeu de test pour musees"() {
+    JeuTestService jeuTestService
+
+
+    void "test creation jeu de tests pour musees"() {
+
+        given: "une base ne contenant pas de musee"
+        Musee.count() == 0
+
+        when: "on crée le jeu de test pour les musee"
+        jeuTestService.createJeuTestAdresse()
+        jeuTestService.createJeuTestGestionnaire()
+        jeuTestService.createJeuTestMusee()
+
+        then: "12 nouveaux musees ont été crées en base"
+        Musee.count() == 12
+
+
+        when:" des musee exitent deja dans la base"
+        Musee.count() == 12
+
+        and:"on déclenche à nouveau la création du jeu de test pour musees"
+        jeuTestService.createJeuTestAdresse()
+        jeuTestService.createJeuTestGestionnaire()
+        jeuTestService.createJeuTestMusee()
+
+        then:"aucun nouveau musee n'est crée"
+        Musee.count() == 12
+
     }
+
 }
